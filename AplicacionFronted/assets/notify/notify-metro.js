544b736c0c27a9b653585271be31a29a2055834c
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2017-08-06 11:52:01 -0500

    Empezando con Frontend

---
+++ AplicacionFronted/assets/notify/notify-metro.js
@@ -0,0 +1,39 @@
+$.notify.addStyle("metro", {
+    html:
+        "<div>" +
+            "<div class='image' data-notify-html='image'/>" +
+            "<div class='text-wrapper'>" +
+                "<div class='title' data-notify-html='title'/>" +
+                "<div class='text' data-notify-html='text'/>" +
+            "</div>" +
+        "</div>",
+    classes: {
+        error: {
+            "color": "#fafafa !important",
+            "background-color": "#a53434",
+            "border": "1px solid #FF0026"
+        },
+        success: {
+            "background-color": "#34a58e",
+            "border": "1px solid #4DB149"
+        },
+        info: {
+            "color": "#fafafa !important",
+            "background-color": "#3488a5",
+            "border": "1px solid #1E90FF"
+        },
+        warning: {
+            "background-color": "#FAFA47",
+            "border": "1px solid #EEEE45"
+        },
+        black: {
+            "color": "#fafafa !important",
+            "background-color": "#333",
+            "border": "1px solid #000"
+        },
+        white: {
+            "background-color": "#f1f1f1",
+            "border": "1px solid #ddd"
+        }
+    }
+});
---
+++ AplicacionFronted/controllers/notify.js
@@ -0,0 +1,86 @@
+// ###########################################################
+// Muestra notificacion, titulo, texto, tipo
+// ###########################################################
+$.showNotify = function($title, $text, $style, $position) {
+  if($style == "error"){
+    $icon = "fa fa-exclamation";
+  }else if($style == "warning"){
+    $icon = "fa fa-warning";
+  }else if($style == "success"){
+    $icon = "fa fa-check";
+  }else if($style == "info"){
+    $icon = "fa fa-question";
+  }else{
+    $icon = "fa fa-circle-o";
+  }
+  $.notify({
+    title: $title,
+    text: $text,
+    image: "<i class='"+$icon+"'></i>"
+  }, {
+    style: 'metro',
+    className: $style,
+    globalPosition:$position,
+    showAnimation: "show",
+    showDuration: 0,
+    hideDuration: 0,
+    autoHideDelay: 8000,
+    autoHide: true,
+    clickToHide: true
+  });
+}
+
+
+// ###########################################################
+// texto a mostrar en el mnsje, metodo si le da click a si, y los parametros
+//que tendria el meotod,y el estilo de la notifcacion
+// ###########################################################
+$.showConfirm = function($title, $text, $method, $parameterMethodY, $parameterMethodN, $style){
+  $style || ( $style = 'warning' );
+
+  if($style == "error"){
+    $icon = "fa fa-exclamation";
+  }else if($style == "warning"){
+    $icon = "fa fa-warning";
+  }else if($style == "success"){
+    $icon = "fa fa-check";
+  }else if($style == "info"){
+    $icon = "fa fa-question";
+  }else{
+    $icon = "fa fa-circle-o";
+  }
+
+  $.notify({
+    title: $title,
+    text: $text+'<div class="clearfix"></div><br><button onclick="'+$method+'('+$parameterMethodY+'); return false" class="notify__hidden">Sí</button> <button onclick="'+$method+'('+$parameterMethodN+'); return false" class="notify__hidden">No</button>',
+    image: "<i class='"+$icon+"'></i>"
+  }, {
+    style: 'metro',
+    className: $style,
+    showAnimation: "show",
+    showDuration: 0,
+    hideDuration: 0,
+    autoHideDelay: 30000,
+    autoHide: false,
+    clickToHide: true
+  });
+}
+
+
+// ###########################################################
+// Muestra animacion de carga
+// ###########################################################
+$.showLoading = function($text){
+  $('body').css('overflow','hidden');
+  $('.popup__loading').addClass('active');
+  $.showNotify('Estado', $text, 'info');
+}
+
+
+// ###########################################################
+// Oculta animacion de carga
+// ###########################################################
+$.hiddenLoading = function(){
+  $('body').css('overflow','auto');
+  $('.popup__loading').removeClass('active');
+}
